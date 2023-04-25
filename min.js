9f4be723bdcb5d7e563a9a75d966c6ba19ef9d3e
mohammed hamada <131654026+mohammedhamada13@users.noreply.github.com>
2023-04-25 02:57:35 +0200

    Add files via upload

---
+++ min.js
@@ -0,0 +1,11 @@
+let theCode = document.getElementById('code');
+let container = document.querySelector('.container');
+
+
+function hellos(hello) {
+    theCode.src = hello;
+}
+
+function colors(color) {
+    container.style.background = color;
+}
